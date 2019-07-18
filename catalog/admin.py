3aa53d72f485082edfd1eca6d0be76a9b3b0ceaa
Mandelai <ricardo.aidelman@gmail.com>
2019-07-18 19:28:28 -0300

    Creating our home page

--- catalog/admin.py
+++ catalog/admin.py
@@ -12,13 +12,20 @@ admin.site.register(Language)
 
 
 # Define the admin class
+class BookInline(admin.TabularInline):
+   model = Book
+   extra = 0
+   can_delete = False
+   fields =  ('title', 'summary', 'genre', 'isbn' )
+   # list_display = ('title', 'summary', 'display_genre', 'isbn' )
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
-
 
 class BooksInstanceInline(admin.TabularInline):
    model = BookInstance
