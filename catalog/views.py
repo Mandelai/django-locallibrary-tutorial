35d289008cd40d8be1727688894b238aa8b8eae3
Mandelai <ricardo.aidelman@gmail.com>
2019-07-19 20:56:09 -0300

    Sessions framework

--- catalog/views.py
+++ catalog/views.py
@@ -19,6 +19,10 @@ def index(request):
     num_genres      = Genre.objects.count()
     num_languages   = Language.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books'                 : num_books,
         'num_instances'             : num_instances,
@@ -26,8 +30,9 @@ def index(request):
         'num_authors'               : num_authors,
         'num_genres'                : num_genres,
         'num_languages'             : num_languages,
-    }
-
+        'num_visits': num_visits,
+        }
+        
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
@@ -39,7 +44,7 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
-    
+
 class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 10
